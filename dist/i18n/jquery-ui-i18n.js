64cbc812326b5a592a8efd601b4a8bd812e568af
Sobieslaw Hrehorowicz <sobieslaw.hrehorowicz@xstream.net>
2015-06-19 17:21:49 +0200

    Core: custom build

---
+++ dist/i18n/jquery-ui-i18n.js
@@ -0,0 +1,117 @@
+/*! jQuery UI - v1.12.0-pre - 2015-06-19
+* http://jqueryui.com
+* Includes: datepicker-en-AU.js, datepicker-en-GB.js, datepicker-en-NZ.js
+* Copyright jQuery Foundation and other contributors; Licensed MIT */
+/* English/Australia initialisation for the jQuery UI date picker plugin. */
+/* Based on the en-GB initialisation. */
+(function( factory ) {
+	if ( typeof define === "function" && define.amd ) {
+
+		// AMD. Register as an anonymous module.
+		define([ "../datepicker" ], factory );
+	} else {
+
+		// Browser globals
+		factory( jQuery.datepicker );
+	}
+}(function( datepicker ) {
+
+datepicker.regional['en-AU'] = {
+	closeText: 'Done',
+	prevText: 'Prev',
+	nextText: 'Next',
+	currentText: 'Today',
+	monthNames: ['January','February','March','April','May','June',
+	'July','August','September','October','November','December'],
+	monthNamesShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+	'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+	dayNames: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+	dayNamesShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+	dayNamesMin: ['Su','Mo','Tu','We','Th','Fr','Sa'],
+	weekHeader: 'Wk',
+	dateFormat: 'dd/mm/yy',
+	firstDay: 1,
+	isRTL: false,
+	showMonthAfterYear: false,
+	yearSuffix: ''};
+datepicker.setDefaults(datepicker.regional['en-AU']);
+
+return datepicker.regional['en-AU'];
+
+}));
+
+/* English/UK initialisation for the jQuery UI date picker plugin. */
+/* Written by Stuart. */
+(function( factory ) {
+	if ( typeof define === "function" && define.amd ) {
+
+		// AMD. Register as an anonymous module.
+		define([ "../datepicker" ], factory );
+	} else {
+
+		// Browser globals
+		factory( jQuery.datepicker );
+	}
+}(function( datepicker ) {
+
+datepicker.regional['en-GB'] = {
+	closeText: 'Done',
+	prevText: 'Prev',
+	nextText: 'Next',
+	currentText: 'Today',
+	monthNames: ['January','February','March','April','May','June',
+	'July','August','September','October','November','December'],
+	monthNamesShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+	'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+	dayNames: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+	dayNamesShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+	dayNamesMin: ['Su','Mo','Tu','We','Th','Fr','Sa'],
+	weekHeader: 'Wk',
+	dateFormat: 'dd/mm/yy',
+	firstDay: 1,
+	isRTL: false,
+	showMonthAfterYear: false,
+	yearSuffix: ''};
+datepicker.setDefaults(datepicker.regional['en-GB']);
+
+return datepicker.regional['en-GB'];
+
+}));
+
+/* English/New Zealand initialisation for the jQuery UI date picker plugin. */
+/* Based on the en-GB initialisation. */
+(function( factory ) {
+	if ( typeof define === "function" && define.amd ) {
+
+		// AMD. Register as an anonymous module.
+		define([ "../datepicker" ], factory );
+	} else {
+
+		// Browser globals
+		factory( jQuery.datepicker );
+	}
+}(function( datepicker ) {
+
+datepicker.regional['en-NZ'] = {
+	closeText: 'Done',
+	prevText: 'Prev',
+	nextText: 'Next',
+	currentText: 'Today',
+	monthNames: ['January','February','March','April','May','June',
+	'July','August','September','October','November','December'],
+	monthNamesShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+	'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+	dayNames: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+	dayNamesShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+	dayNamesMin: ['Su','Mo','Tu','We','Th','Fr','Sa'],
+	weekHeader: 'Wk',
+	dateFormat: 'dd/mm/yy',
+	firstDay: 1,
+	isRTL: false,
+	showMonthAfterYear: false,
+	yearSuffix: ''};
+datepicker.setDefaults(datepicker.regional['en-NZ']);
+
+return datepicker.regional['en-NZ'];
+
+}));
